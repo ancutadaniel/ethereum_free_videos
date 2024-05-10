fabec385c58ee7864c9d8e418efd212b7d643b22
Daniel Ancuta <dacether@dacether.local>
2024-05-10 17:23:58 +0300

    layout header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-
 import { useConnectWallet } from "@web3-onboard/react";
 import logo from "../../assets/logo.webp";
 import Button from "../UI/Button";
@@ -12,21 +11,21 @@ const Header: React.FC = () => {
   const walletAddress = wallet?.accounts[0]?.address;
 
   return (
-    <header className="bg-white dark:bg-slate-700 shadow-md">
+    <header className="bg-background-default dark:bg-background-dark shadow-md">
       <div className="container mx-auto flex justify-between items-center p-4">
         <img src={logo} alt="Logo" className="h-10 w-auto" />
         <div className="hidden md:flex items-center space-x-4">
-          <Button onClick={toggleTheme}>{themeIcon}</Button>
-          <Button onClick={() => connect()} className="bg-green-500 text-white">
+          {/* Updated Button for Toggle Theme */}
+          <Button onClick={toggleTheme} className="bg-secondary-dark text-yellow-300 dark:text-yellow-300 dark:bg-transparent">
+            {themeIcon}
+          </Button>
+          <Button onClick={() => connect()} className="dark:bg-accent-orange text-white">
             {walletAddress
               ? `${walletAddress.slice(0, 4)}...${walletAddress.slice(-4)}`
               : "Connect Wallet"}
           </Button>
           {wallet && (
-            <Button
-              onClick={() => disconnect(wallet)}
-              className="bg-red-500 text-white"
-            >
+            <Button onClick={() => disconnect(wallet)} className="bg-secondary-dark text-white">
               Disconnect
             </Button>
           )}
--- src/components/UI/Button.tsx
+++ src/components/UI/Button.tsx
@@ -17,13 +17,11 @@ const Button: React.FC<ButtonProps> = ({
   const { theme } = useTheme();
 
   // Basic styles for all buttons
-  const baseStyle =
-    "px-4 py-2 rounded-lg shadow-lg transition-all duration-300";
+  const baseStyle = "px-4 py-2 rounded-lg shadow-lg transition-all duration-300";
   // Conditional theme-based styles, adjusted based on the context
-  const themeStyle =
-    theme === "light"
-      ? "bg-red-500 text-white hover:bg-blue-400 focus:ring-blue-300"
-      : "bg-green-800 text-yellow-300 hover:bg-gray-700 focus:ring-gray-500";
+  const themeStyle = theme === "light"
+      ? "bg-primary-light text-text-light hover:bg-primary hover:focus:ring-primary"
+      : "bg-primary text-text-dark hover:bg-primary-dark focus:ring-primary-dark";
 
   // Text-only button minimal styling
   const textOnlyStyle = textOnly ? "bg-transparent shadow-none" : themeStyle;
