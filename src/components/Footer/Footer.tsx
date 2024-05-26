8e689b6ce0ee02ae0e4cf51e18357bf56c931994
Daniel Ancuta <dacether@dacether.local>
2024-05-26 18:30:22 +0300

    update toggle and footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,7 +3,9 @@ import React from 'react';
 
 const Footer: React.FC = () => {
   return (
-    <footer className="p-4 bg-gray-800 text-white text-center flex-none">
+    <footer
+    
+    className="p-4 bg-background-default dark:bg-background-dark text-dark dark:text-white text-center flex-none">
       &copy; {new Date().getFullYear()}
     </footer>
   );
